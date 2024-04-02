5e3e0a9a273b3d6877bcec925a0e3d0418ae5f5c
Darth Degen <darthdegen@gmail.com>
2024-04-01 18:07:41 -0700

    feaet: updating background styles

--- src/components/templates/LandingView.tsx
+++ src/components/templates/LandingView.tsx
@@ -15,7 +15,7 @@ const LandingView: FC<Props> = (props: Props) => {
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
-      <motion.div {...fadeAnimation(showView)}>
+      <motion.div {...fadeAnimation(showView, 0.5)}>
         <Image
           src="/images/logo-lg.png"
           alt="Cyber Frogs"
@@ -27,7 +27,7 @@ const LandingView: FC<Props> = (props: Props) => {
       </motion.div>
       <motion.div
         className="pt-4 uppercase font-rajdhani-bold text-xl text-center"
-        {...fadeAnimation(showView, 1)}
+        {...fadeAnimation(showView, 1.5)}
       >
         Presented by slimes studio
       </motion.div>
