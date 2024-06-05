9253cbfee04bc934e4f951fb81a6f3970bf6ed9a
Darth Degen <darthdegen@gmail.com>
2024-06-05 06:22:31 -0700

    feat: raffle image display

--- apps/raffles/components/templates/RafflesView.tsx
+++ apps/raffles/components/templates/RafflesView.tsx
@@ -4,6 +4,8 @@ import { Heading } from "@components";
 import { handleAssetLoad } from "@utils";
 import { raffles } from "@raffles-constants";
 import { Raffles } from "@raffles-types";
+import { AnimatePresence, motion } from "framer-motion";
+import { midExitAnimation } from "src/constants";
 
 interface Props {
   setAssets: Dispatch<SetStateAction<boolean[]>>;
@@ -12,21 +14,43 @@ interface Props {
 const RafflesView: FC<Props> = (props: Props) => {
   const { setAssets } = props;
 
+  const showRaffles = raffles.length > 0;
+
   return (
-    <div className="w-full flex flex-col items-center xl:items-start justify-start px-3 md:px-12 2xl:px-0 my-10 ">
+    <div className="w-full flex flex-col px-3 md:px-12 2xl:px-0 mt-10 ">
       {/* heading & image */}
-      <div className="relative w-full flex flex-row justify-between px-3 md:px-0 2xl:ml-4">
+      <div className="relative w-full flex flex-col xl:flex-row justify-between px-3 md:px-0 2xl:ml-4">
         <Heading />
-        <Image
-          src="/images/raffles/intro.jpg"
-          width={1017}
-          height={208}
-          alt="Raffles"
-          // className="hidden md:block absolute right-0 -top-4 z-0 opacity-20 xl:opacity-80 2xl:opacity-100"
-          className="hidden xl:block xl:opacity-80 2xl:opacity-100 -mt-10 mx-5"
-          onLoad={() => handleAssetLoad(0, setAssets)}
-        />
+
+        {showRaffles && (
+          <Image
+            src="/images/raffles/intro.jpg"
+            width={1017}
+            height={208}
+            alt="Raffles"
+            className="hidden xl:block xl:opacity-80 2xl:opacity-100 -mt-10 mx-5"
+            // onLoad={() => handleAssetLoad(0, setAssets)}
+          />
+        )}
       </div>
+      {/* raffles */}
+      <AnimatePresence mode="wait">
+        {!showRaffles ? (
+          <motion.div key="no-raffles" {...midExitAnimation}>
+            <Image
+              src="/images/raffles/empty.png"
+              width={1219}
+              height={860}
+              alt="Mp Raffles"
+              className="mt-20 lg:-mt-20 lg:p-10"
+            />
+          </motion.div>
+        ) : (
+          <motion.div key="raffles" {...midExitAnimation} className="my-10">
+            Raffles
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
