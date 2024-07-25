7973126bd1642357bf5a6e7c77b28eacd2c0782e
Darth Degen <darthdegen@gmail.com>
2024-07-25 10:10:28 -0700

    dashboard update

--- apps/dashboard/components/molecules/DataWrapper.tsx
+++ apps/dashboard/components/molecules/DataWrapper.tsx
@@ -9,13 +9,9 @@ const DataWrapper: FC<DataProps> = (props: DataProps) => {
   const { title, children } = props;
   return (
     <div className="relative w-full sm:w-[256px] min-w-[200px] md:min-w-[256px] h-[172px] min-h-[172px] flex flex-col py-4">
-      <Image
-        src="/images/pages/dashboard/divider.svg"
-        width={256}
-        height={1}
-        alt="Divider"
-        className="-mt-4 mb-4 opacity-75 w-full"
-      />
+      <div className="realtive w-full h-[1px] shadow-inner bg-cf-white/15 -mt-4 mb-4 ">
+        <div className="absolute left-0 h-[1px] w-3 bg-cf-gold-500/30"></div>
+      </div>
       <p className="uppercase mb-6">{title}</p>
       {children}
     </div>
