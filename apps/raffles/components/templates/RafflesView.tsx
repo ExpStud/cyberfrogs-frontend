de99fc3931ef0d9d8ab83161a04ca2ff48271cd5
Darth Degen <darthdegen@gmail.com>
2024-07-25 10:27:25 -0700

    mobile raffles, added data back

--- apps/raffles/components/templates/RafflesView.tsx
+++ apps/raffles/components/templates/RafflesView.tsx
@@ -13,7 +13,7 @@ interface Props {
 const RafflesView: FC<Props> = (props: Props) => {
   const { setAssets } = props;
 
-  const showRaffles = raffles.length === 0; //TODO: raffles.length > 0;
+  const showRaffles = raffles.length > 0;
 
   return (
     <div className="w-full flex flex-col px-3 md:px-12 2xl:px-0 mt-10 ">
